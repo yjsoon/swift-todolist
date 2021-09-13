97463055b4a48cfa7f8be2d76797e4368b4f0919
YJ Soon <yjsoon@tinkertanker.com>
2021-09-13 17:55:15 +0800

    Can tap on todo item and toggle it

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -26,6 +26,12 @@ struct ContentView: View {
                         .strikethrough(todo.isCompleted)
                         .foregroundColor(todo.isCompleted ? .gray : .black)
                 }
+                .onTapGesture {
+                    let todoIndex = todos.firstIndex {
+                        $0.id == todo.id
+                    }!
+                    todos[todoIndex].isCompleted.toggle()
+                }
             }
             .navigationTitle("Todos")
         }
